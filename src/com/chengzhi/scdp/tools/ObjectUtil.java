b702a071357fe27ccad30789ec736bf51bd1f1dd
caochengzhi <beisi85473795@163.com>
2017-11-11 00:23:00 +0800

    commit

---
+++ src/com/chengzhi/scdp/tools/ObjectUtil.java
@@ -0,0 +1,182 @@
+package com.chengzhi.scdp.tools;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
+
+public class ObjectUtil {
+	private static Logger logger = Logger.getLogger(ObjectUtil.class);
+	
+	/**
+	 * 构造查询条件
+	 * @param c
+	 * @param cond
+	 */
+	public static void contructCond(Criteria c,Object cond){
+		Map<String,Field> fieldMap = BaseDataCache.getClassFieldsMap(cond.getClass());
+		for(Iterator<Field> it = fieldMap.values().iterator();it.hasNext();){
+			Field field = it.next();
+			Method method = BaseDataCache.getGetMethod(cond.getClass(), field.getName());
+			setCondValue(method, field, cond, c);
+		}
+	}
+	
+	/**
+	 * 设置查询条件
+	 * @param method
+	 * @param field
+	 * @param cond
+	 * @param c
+	 */
+	private static void setCondValue(Method method,Field field,Object cond,Criteria c){
+		String type = field.getType().getName();
+		try{
+			String fieldName = field.getName().toLowerCase();
+			Object obj = method.invoke(cond, null);
+			if(obj == null)
+				return;
+			
+			String value = (String)obj;
+			if(fieldName.endsWith("notisnull")){
+				if("Y".equalsIgnoreCase(value)){
+					String fieldNameNotIsNull = field.getName();
+					String _fieldName  = fieldNameNotIsNull.substring(0, fieldNameNotIsNull.length()-9);
+					c.add(Restrictions.isNotNull(_fieldName));
+				}
+			}
+			else if(fieldName.endsWith("isnull") && !fieldName.endsWith("notisnull")){
+				if("Y".equalsIgnoreCase(value)){
+					String fieldNameIsNull = field.getName();
+					String _fieldName = fieldNameIsNull.substring(0, fieldNameIsNull.length()-6);
+					c.add(Restrictions.isNull(_fieldName));
+				}
+			}
+			else if("java.lang.String".equals(type)){
+				String _fieldName = field.getName();
+				if(fieldName.endsWith("$like")){
+					if(!value.startsWith("%"))
+						value = "%" + value;
+					if(!value.endsWith("%"))
+						value = value + "%";
+					c.add(Restrictions.like(_fieldName.substring(0, _fieldName.length()-5), value).ignoreCase());
+				}
+				else{
+					c.add(Restrictions.eq(_fieldName, value).ignoreCase());
+				}
+			}
+			else if("java.lang.Integer".equals(type)){
+				c.add(Restrictions.eq(field.getName(), (Integer)obj));
+			}
+			else if("java.lang.Float".equals(type)){
+				c.add(Restrictions.eq(field.getName(), (Float)obj));
+			}
+			else if("java.lang.Double".equals(type)){
+				c.add(Restrictions.eq(field.getName(), (Double)obj));
+			}
+			else if("java.util.Date".equals(type)){
+				String _fieldName = field.getName();
+				if(fieldName.endsWith("1")){
+//					c.add(Restrictions.ge(_fieldName.substring(0, _fieldName.length()-1),DateTimeUtil.getDayEarliest((Date)obj)));
+					c.add(Restrictions.ge(_fieldName.substring(0, _fieldName.length()-1), (Date)obj));
+				}
+				else if(fieldName.endsWith("2")){
+//					c.add(Restrictions.le(_fieldName.substring(0, _fieldName.length()-1),DateTimeUtil.getDayLatest((Date)obj)));
+					c.add(Restrictions.le(_fieldName.substring(0, _fieldName.length()-1), (Date)obj));
+				}
+				else{
+					c.add(Restrictions.eq(_fieldName, (Date)obj));
+				}
+			}
+			else if("java.sql.Timestamp".equals(type)){
+				String _fieldName = field.getName();
+				if(fieldName.endsWith("1")){
+					c.add(Restrictions.ge(_fieldName.substring(0, _fieldName.length()-1),DateTimeUtil.getDayEarliest((Timestamp)obj)));
+				}
+				else if(fieldName.endsWith("2")){
+					c.add(Restrictions.le(_fieldName.substring(0, _fieldName.length()-1),DateTimeUtil.getDayLatest((Timestamp)obj)));
+				}
+				else{
+					c.add(Restrictions.eq(_fieldName, (Timestamp)obj));
+				}
+			}
+			else if("java.lang.Boolean".equals(type)){
+				c.add(Restrictions.eq(field.getName(), (Boolean)obj));
+			}
+			else if("java.math.BigDecimal".equals(type)){
+				String _fieldName = field.getName();
+				if(fieldName.endsWith("1")){
+					c.add(Restrictions.ge(_fieldName.substring(0, _fieldName.length()-1), (BigDecimal)obj));
+				}
+				else if(fieldName.endsWith("2")){
+					c.add(Restrictions.le(_fieldName.substring(0, _fieldName.length()-1),(BigDecimal)obj));
+				}
+				else if(fieldName.endsWith("$notequal")){
+					c.add(Restrictions.not(Restrictions.eq(_fieldName.substring(0, _fieldName.length()-9), (BigDecimal)obj)));
+				}
+				else{
+					c.add(Restrictions.eq(_fieldName, (BigDecimal)obj));
+				}
+			}
+			else if("java.lang.Long".equals(type)){
+				String _fieldName = field.getName();
+				if(fieldName.endsWith("1")){
+					c.add(Restrictions.ge(_fieldName.substring(0, _fieldName.length()-1), (Long)obj));
+				}
+				else if(fieldName.endsWith("2")){
+					c.add(Restrictions.le(_fieldName.substring(0, _fieldName.length()-1),(Long)obj));
+				}
+				else if(fieldName.endsWith("$notequal")){
+					c.add(Restrictions.not(Restrictions.eq(_fieldName.substring(0, _fieldName.length()-9), (Long)obj)));
+				}
+				else{
+					c.add(Restrictions.eq(_fieldName, (Long)obj));
+				}
+			}
+			else if("[Ljava.lang.Long;".equalsIgnoreCase(type)){
+				String _fieldName = field.getName();
+				if(fieldName.endsWith("notin")){
+					c.add(Restrictions.not(Restrictions.in(_fieldName.substring(0, _fieldName.length()-5), (Long[])obj)));
+				}
+				else if(fieldName.endsWith("in")){
+					c.add(Restrictions.in(_fieldName.substring(0, _fieldName.length()-2), (Long[])obj));
+				}
+			}
+			else if("[Ljava.lang.String".equalsIgnoreCase(type)){
+				String _fieldName = field.getName();
+				if(fieldName.endsWith("notin")){
+					c.add(Restrictions.or(Restrictions.isNull(_fieldName.substring(0, _fieldName.length()-5))
+							,Restrictions.not(Restrictions.in(_fieldName.substring(0, _fieldName.length()-5)
+									, (String[])obj))));
+				}
+				else if(fieldName.endsWith("between")){
+					c.add(Restrictions.between(_fieldName.substring(0, _fieldName.length()-7), ((String[])obj)[0], ((String[])obj)[1]));
+				}
+				else if(fieldName.endsWith("in")){
+					c.add(Restrictions.in(_fieldName.substring(0, _fieldName.length()-2), (String[])obj));
+				}
+			}
+			
+		}catch(Exception e){
+			StringUtil.writeStackTraceToLog(logger, e);
+		}
+	}
+	
+	/**
+	 * 对象拷贝
+	 * @param dest 目标对象
+	 * @param orig 源对象
+	 */
+	public static void copyProperties(Object dest,Object orig)throws Exception{
+		PropertyUtils.copyProperties(dest, orig);
+	}
+
+}
